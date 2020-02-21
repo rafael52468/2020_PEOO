8f04b51338baa842c6d29b2dffb6a2985eb93b0b
Rafael Ribeiro de Lima <lima.ribeiro@academico.ifrn.edu.br>
2020-02-21 15:46:43 -0300

    Versão 1

---
+++ Lista1PEOO/Exercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota dos dois bimestres da disciplina");
+            double nota1 = double.Parse(Console.ReadLine());
+            double nota2 = double.Parse(Console.ReadLine());
+            double media = (nota1 * 2 + nota2 * 3) / 5;
+            Console.WriteLine($"Média parcial = {media}");
+
+        }
+    }
+}
